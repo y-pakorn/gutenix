0244206ba3b4a5b8f9c3914e6debbcaf5daf53c2
Pakorn Nathong <pakorn.nth@hotmail.com>
2024-11-15 22:10:06 +0700

    feat: update config

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -49,7 +49,6 @@ export const metadata: Metadata = {
     title: siteConfig.name,
     description: siteConfig.description,
     images: [siteConfig.ogImage],
-    creator: "@_rdev7",
   },
   icons: {
     icon: "/favicon.ico",
--- frontend/src/config/site.ts
+++ frontend/src/config/site.ts
@@ -3,17 +3,16 @@ import { SiteConfig } from "@/types"
 import { env } from "@/env.mjs"
 
 export const siteConfig: SiteConfig = {
-  name: "Next Entree",
-  author: "redpangilinan",
-  description:
-    "Next.js 14+ starter template with app router, shadcn/ui, typesafe env, icons and configs setup.",
-  keywords: ["Next.js", "React", "Tailwind CSS", "Radix UI", "shadcn/ui"],
+  name: "Redacted",
+  author: "yoisha & yoyoismee",
+  description: "",
+  keywords: [],
   url: {
     base: env.NEXT_PUBLIC_APP_URL,
-    author: "https://rdev.pro",
+    author: "yoisha & yoyoismee",
   },
   links: {
-    github: "https://github.com/redpangilinan/next-entree",
+    github: "https://github.com/y-pakorn/redacted",
   },
   ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.jpg`,
 }
